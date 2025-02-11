6cade0434b5ee86e8ae69d3bd7082282e9a3f4ea
Ansees Adhikari <anseesadhikari@gmail.com>
2025-02-11 12:03:05 +0545

    the first c programme

---
+++ age.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf( "enter your age:");
+scanf("%d",&a);
+if(a>=19)
+{
+    printf("the person is egible to vote");
+
+}
+else
+{
+    printf("the person is not egible to vote");
+}
+
+}
